8523f241fd8b0a9035095219d3265a503ff88e9c
oki <h30e07@gmail.com>
2024-03-23 15:57:05 +0900

    add contour finding function

--- src/cam_detect_obj.py
+++ src/cam_detect_obj.py
@@ -18,17 +18,23 @@ PADDY_RICE_RADIUS = 100.0
 # 検出可能最大距離[mm]
 DETECTABLE_MAX_DIS = 10000.0
 
+# 検出した輪郭の最小面積[pxl]
+MIN_CONTOUR_AREA_THRESHOLD = 60
+
+# 円形度の閾値
+CIRCULARITY_THRESHOLD=0.5
+
 # ロボットの中心位置を原点とした時のカメラの位置[mm]
 CAMERA_POS_X = 100
 CAMERA_POS_Y = 400
 CAMERA_POS_Z = 150
 
-# ロボット座標におけるカメラの傾き[rad]
+# カメラ座標におけるカメラの傾き[rad]
 theta_x = 30*np.pi/180
 theta_y = 0
 theta_z = 0
 
-# アームのファンで吸い込めるエリアの中心と半径[mm]
+# ロボット座標におけるアームのファンで吸い込めるエリアの中心と半径[mm]
 OBTAINABE_AREA_CENTER_X = 0
 OBTAINABE_AREA_CENTER_Y = 550
 OBTAINABE_AREA_RADIUS = 80
@@ -128,11 +134,15 @@ def coordinate_transformation(w, h, dis):
 class FrontCamera:
     def __init__(self, device_id):
         # Camera Settings
-        self.cap = cv2.VideoCapture(device_id, cv2.CAP_V4L2)
+        # self.cap = cv2.VideoCapture(device_id, cv2.CAP_V4L2)
+        self.cap = cv2.VideoCapture(device_id)
         self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
         self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        set_fps = self.cap.set(cv2.CAP_PROP_FPS, 30)
+        print(f"device_id_{device_id} fps:{self.cap.get(cv2.CAP_PROP_FPS)}, {set_fps}")
+        
         
     def read(self):
         ret, frame = self.cap.read()
@@ -143,8 +153,7 @@ class FrontCamera:
         print("Closed Capturing Device")
     
     def isOpened(self):
-        return self.cap.isOpened()
-        
+        return self.cap.isOpened()    
 
 class MainProcess:
     def __init__(self, model_path):
@@ -167,8 +176,8 @@ class MainProcess:
             try:
                 ret, frame = cap.read()
                 if not ret:
-                    #frame = np.zeros((FRAME_HEIGHT, FRAME_WIDTH, 3))
-                    break  
+                    # frame = np.zeros((FRAME_HEIGHT, FRAME_WIDTH, 3))
+                    continue
                 q_frames.put(frame)
                 print(f"read frame")
             except KeyboardInterrupt:
@@ -180,67 +189,51 @@ class MainProcess:
             try:
                 frame = q_frames.get()
                 
-                # グレースケール化
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                
                 # カメラ画像をHSVに変換
                 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
                 
+                # メディアンフィルタを適用する。
+                bimg = cv2.medianBlur(hsv, ksize=9)
+
                 # 指定したHSVの値でマスクを作成する
-                mask = cv2.inRange(hsv, self.blue_lower_mask, self.blue_upper_mask)
+                mask = cv2.inRange(bimg, self.blue_lower_mask, self.blue_upper_mask)
+                
                 # マスクを反転
                 # mask = cv2.bitwise_not(mask)
+                # グレースケール化
+                # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 # マスクを除いたグレースケール画像を生成
                 # gray = cv2.bitwise_or(gray, mask)
-                
-                # メディアンフィルタを適用する。
-                #bimg = cv2.medianBlur(gray, ksize=9)
-                
-                # 輪郭検出
-                contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                
-                if len(contours) > 0:
-                    for i, cnt in enumerate(contours):
-                        if calc_circularity(cnt)>0.4:
-                            (x,y),r = cv2.minEnclosingCircle(cnt)
-                            cv2.circle(frame,(int(x),int(y)),int(r),(0,255,0),2)
-                q_results.put((frame, len(contours), x, y, r, False))
-                
-                """
-                # ハフ変換
-                circles = cv2.HoughCircles(bimg,cv2.HOUGH_GRADIENT,1,100,param1=100,param2=30,minRadius=0,maxRadius=int(FRAME_WIDTH/3))
                 
                 paddy_rice_x = 0
                 paddy_rice_y = 0
                 paddy_rice_z = DETECTABLE_MAX_DIS
                 is_obtainable = False
-                try:
-                    circles = np.uint16(np.around(circles))
-                    
-                    #デバッグ用　画面に円を表示する準備
-                    for i in circles[0,:]:
-                        cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
-                    
-                    color_boxes = [frame[h-r:h+r,w-r:w+r] for w,h,r in zip(circles[0,:,0],circles[0,:,1],circles[0,:,2])]
-                    Hues = [color_box.T[0].flatten().mean() for color_box in color_boxes]
-                    print(Hues)
-                    target = np.argmax(circles[0,:,2], axis=0)
-                    (paddy_rice_x, paddy_rice_y, paddy_rice_z) = coordinate_transformation(int(circles[0,target,0]), int(circles[0,target,1]), calc_distance(circles[0,target,2]))
-                    is_obtainable = (paddy_rice_x-OBTAINABE_AREA_CENTER_X)**2 + (paddy_rice_y-OBTAINABE_AREA_CENTER_Y)**2 < OBTAINABE_AREA_RADIUS**2
+
+                # 輪郭検出
+                contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                if len(contours) > 0:
+                    # 小さい輪郭は誤検出として削除
+                    contours = list(filter(lambda x: cv2.contourArea(x) > MIN_CONTOUR_AREA_THRESHOLD, contours))
+
+                    circles = [cv2.minEnclosingCircle(cnt) for cnt in contours if calc_circularity(cnt)>CIRCULARITY_THRESHOLD]
+                    if len(circles) > 0:
+
+                        # デバッグ用に円を描画
+                        [cv2.circle(frame,(int(c[0][0]),int(c[0][1])),int(c[1]),(0,255,0),2) for c in circles]
+                        
+                        target = circles.index(max(circles, key=lambda x:x[1]))
+                        (paddy_rice_x,paddy_rice_y,paddy_rice_z) = coordinate_transformation(int(circles[target][0][0]),int(circles[target][0][1]),calc_distance(circles[target][1]))
+                        is_obtainable = (paddy_rice_x-OBTAINABE_AREA_CENTER_X)**2 + (paddy_rice_y-OBTAINABE_AREA_CENTER_Y)**2 < OBTAINABE_AREA_RADIUS**2
                 
-                except TypeError:
-                    # len(circles)が検出数になるように
-                    circles = np.array([[]])
-                
-                # 検出したボールの座標をキューに送信 (xは水平，yは奥行方向)
-                q_results.put((frame, len(circles[0]), paddy_rice_x, paddy_rice_y, paddy_rice_z, is_obtainable))
-                print(f"image processing")
-                """
+                # 画像のタイプを揃える
+                show_mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+                show_frame = np.hstack((frame, show_mask))
+                q_results.put((show_frame, len(circles), paddy_rice_x, paddy_rice_y, paddy_rice_z, is_obtainable))
                 
             except KeyboardInterrupt:
                 break
     
-    """
     # 推論してキューに入れる
     def inference(self, q_frames, q_results):
         while True:
@@ -273,6 +266,69 @@ class MainProcess:
                 
             except KeyboardInterrupt:
                 break
+    
+    # カメラからの画像取得と推論をスレッドごとに分けて実行      
+    def thread_start(self, cap):
+        thread1 = threading.Thread(target=self.capturing, args=(self.q_frames, cap), daemon=True)
+        thread2 = threading.Thread(target=self.image_processing, args=(self.q_frames, self.q_results), daemon=True)
+        thread1.start()
+        print("thread1 start")
+        thread2.start()
+        print("thread2 start")
+    
+    # キューを空にする
+    def finish(self):
+        while True:
+            try:
+                self.q_frames.get_nowait()
+            except queue.Empty:
+                break
+        while True:
+            try:
+                self.q_results.get_nowait()
+            except queue.Empty:
+                break
+        print("queue empty")
+
+    """
+    # マルチプロセスで実行  
+    def process_start(self, q_results, cap):
+        processes = [multiprocessing.Process(target=self.capturing_and_inference, args=(q_results, cap), daemon=True) for i in range(self.process_num)]
+        for process in processes:
+            process.start()
+            print(f"{process} start")
+    """    
+
+    """
+    # ハフ変換
+    circles = cv2.HoughCircles(bimg,cv2.HOUGH_GRADIENT,1,100,param1=100,param2=30,minRadius=0,maxRadius=int(FRAME_WIDTH/3))
+    
+    paddy_rice_x = 0
+    paddy_rice_y = 0
+    paddy_rice_z = DETECTABLE_MAX_DIS
+    is_obtainable = False
+    try:
+        circles = np.uint16(np.around(circles))
+        
+        #デバッグ用　画面に円を表示する準備
+        for i in circles[0,:]:
+            cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+        
+        color_boxes = [frame[h-r:h+r,w-r:w+r] for w,h,r in zip(circles[0,:,0],circles[0,:,1],circles[0,:,2])]
+        Hues = [color_box.T[0].flatten().mean() for color_box in color_boxes]
+        print(Hues)
+        target = np.argmax(circles[0,:,2], axis=0)
+        (paddy_rice_x, paddy_rice_y, paddy_rice_z) = coordinate_transformation(int(circles[0,target,0]), int(circles[0,target,1]), calc_distance(circles[0,target,2]))
+        is_obtainable = (paddy_rice_x-OBTAINABE_AREA_CENTER_X)**2 + (paddy_rice_y-OBTAINABE_AREA_CENTER_Y)**2 < OBTAINABE_AREA_RADIUS**2
+    
+    except TypeError:
+        # len(circles)が検出数になるように
+        circles = np.array([[]])
+    
+    # 検出したボールの座標をキューに送信 (xは水平，yは奥行方向)
+    q_results.put((frame, len(circles[0]), paddy_rice_x, paddy_rice_y, paddy_rice_z, is_obtainable))
+    print(f"image processing")
+    
     """
     
     """
@@ -312,38 +368,6 @@ class MainProcess:
             except KeyboardInterrupt:
                 break
     """
-    """
-    # マルチプロセスで実行  
-    def process_start(self, q_results, cap):
-        processes = [multiprocessing.Process(target=self.capturing_and_inference, args=(q_results, cap), daemon=True) for i in range(self.process_num)]
-        for process in processes:
-            process.start()
-            print(f"{process} start")
-    """
-    
-    # カメラからの画像取得と推論をスレッドごとに分けて実行      
-    def thread_start(self, cap):
-        thread1 = threading.Thread(target=self.capturing, args=(self.q_frames, cap), daemon=True)
-        thread2 = threading.Thread(target=self.image_processing, args=(self.q_frames, self.q_results), daemon=True)
-        thread1.start()
-        print("thread1 start")
-        thread2.start()
-        print("thread2 start")
-    
-    # キューを空にする
-    def finish(self):
-        while True:
-            try:
-                self.q_frames.get_nowait()
-            except queue.Empty:
-                break
-        while True:
-            try:
-                self.q_results.get_nowait()
-            except queue.Empty:
-                break
-        print("queue empty")
-        
 
 """
 def camera_reader(_cap, out_buf, buf1_ready):
@@ -362,67 +386,67 @@ def camera_reader(_cap, out_buf, buf1_ready):
     Returns
     -----------
 
-  
-    def DetectedObjectCounter(self) -> int:
-        
-        検出したオブジェクト数を返す
 
-        Returns:
-            len(self.boxes) : int
-                検知数
-        
-        
-        try:
-            self.buf1_ready.wait()
-            img = np.reshape(self.buf1, (FRAME_HEIGHT, FRAME_WIDTH, 3))
-            self.buf1_ready.clear()
-            results = self.model.track(img, save=False, imgsz=320, conf=0.5, persist=True, verbose=False)
-            self.annotated_frame = results[0].plot()
-            self.names = results[0].names
-            self.classes = results[0].boxes.cls
-            self.boxes = results[0].boxes
-        finally:
-            return len(self.boxes)
+def DetectedObjectCounter(self) -> int:
+    
+    検出したオブジェクト数を返す
 
-
-
-    def ObjectPosition(self):
-        
-        籾の位置を返す
-
-        Returns:
-            int(self.paddy_rice_x): int
-                ロボット座標の水平方向（カメラ側を前とした時の右が正）[mm]
-            int(self.paddy_rice_y): int
-                ロボット座標の垂直方向（カメラ側を前とした時の上が正）[mm]
-            int(self.paddy_rice_z): int
-                ロボット座標の奥行方向（カメラ側を前とした時の前が正）[mm]
-        
-        try:
-            self.paddy_rice_x = 0
-            self.paddy_rice_y = 0
-            self.paddy_rice_z = DETECTABLE_MAX_DIS
-            for box, cls in zip(self.boxes, self.classes):
-                name = self.names[int(cls)]
-                if(name == "blueball"):
-                    x1, y1, x2, y2 = [int(i) for i in box.xyxy[0]]
-                    # 長方形の長辺を籾の半径とする
-                    r = max(abs(x1-x2), abs(y1-y2))
-                    z = calc_distance(r)
-                    # 籾が複数ある場合は最も近いものの座標を返す
-                    if z < self.paddy_rice_z:
-                        (self.paddy_rice_x, self.paddy_rice_y, self.paddy_rice_z) = coordinate_transformation(int((x1+x2)/2), int((y1+y2)/2), z)
-        finally:
-            return int(self.paddy_rice_x), int(self.paddy_rice_y), int(self.paddy_rice_z)
+    Returns:
+        len(self.boxes) : int
+            検知数
+    
+    
+    try:
+        self.buf1_ready.wait()
+        img = np.reshape(self.buf1, (FRAME_HEIGHT, FRAME_WIDTH, 3))
+        self.buf1_ready.clear()
+        results = self.model.track(img, save=False, imgsz=320, conf=0.5, persist=True, verbose=False)
+        self.annotated_frame = results[0].plot()
+        self.names = results[0].names
+        self.classes = results[0].boxes.cls
+        self.boxes = results[0].boxes
+    finally:
+        return len(self.boxes)
 
 
-    def IsObtainable(self):
-        
-        籾をピックアップできる領域内に籾が存在するかどうか
 
-        Returns
-        ----------
-        領域内ならばTrue
-        そうでないならばFalse
-        
-    """
+def ObjectPosition(self):
+    
+    籾の位置を返す
+
+    Returns:
+        int(self.paddy_rice_x): int
+            ロボット座標の水平方向（カメラ側を前とした時の右が正）[mm]
+        int(self.paddy_rice_y): int
+            ロボット座標の垂直方向（カメラ側を前とした時の上が正）[mm]
+        int(self.paddy_rice_z): int
+            ロボット座標の奥行方向（カメラ側を前とした時の前が正）[mm]
+    
+    try:
+        self.paddy_rice_x = 0
+        self.paddy_rice_y = 0
+        self.paddy_rice_z = DETECTABLE_MAX_DIS
+        for box, cls in zip(self.boxes, self.classes):
+            name = self.names[int(cls)]
+            if(name == "blueball"):
+                x1, y1, x2, y2 = [int(i) for i in box.xyxy[0]]
+                # 長方形の長辺を籾の半径とする
+                r = max(abs(x1-x2), abs(y1-y2))
+                z = calc_distance(r)
+                # 籾が複数ある場合は最も近いものの座標を返す
+                if z < self.paddy_rice_z:
+                    (self.paddy_rice_x, self.paddy_rice_y, self.paddy_rice_z) = coordinate_transformation(int((x1+x2)/2), int((y1+y2)/2), z)
+    finally:
+        return int(self.paddy_rice_x), int(self.paddy_rice_y), int(self.paddy_rice_z)
+
+
+def IsObtainable(self):
+    
+    籾をピックアップできる領域内に籾が存在するかどうか
+
+    Returns
+    ----------
+    領域内ならばTrue
+    そうでないならばFalse
+    
+"""
