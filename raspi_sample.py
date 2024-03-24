0f7e90a1cd9e0a22736a89a15e36e5462361401e
oki <h30e07@gmail.com>
2024-03-24 13:29:11 +0900

    dont show frame

---
+++ raspi_sample.py
@@ -0,0 +1,43 @@
+import cv2
+from src import FrontCamera, MainProcess
+import time
+
+if __name__ == "__main__":
+    #ncnn_model_path = 'models/20240109best_ncnn_model'
+    model_path = 'models/20240109best.pt'
+    
+    # カメラのクラス
+    cam = FrontCamera(0)
+    
+    # メインプロセスを実行するクラス
+    mainprocess = MainProcess(model_path)
+
+    # 処理数
+    count = 0
+    
+    # マルチスレッドの実行
+    mainprocess.thread_start(cam)
+    
+    start_time = time.time()
+    while True:
+        try:
+            _, items, x, y, z, is_obtainable = mainprocess.q_results.get()
+            #_, items, x, y, z, is_obtainable = (1,1,1,1,True)
+            print(f"\nitems:{items}, x:{x}, y:{y}, z:{z}, is_obtainable:{is_obtainable}")
+            count += 1
+            """
+            cv2.drawMarker(frame, (160,128), (0,0,255))
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+            """
+            
+        except KeyboardInterrupt:
+            break
+    cam.release()
+    mainprocess.finish()
+    end_time = time.time()
+    print(f"count / time : {count / (end_time - start_time)}")
+    
+    
+    cv2.destroyAllWindows()
